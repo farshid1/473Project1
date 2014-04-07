ced515158142f48dfcbecf5e7ff7434d31e14776
RazorHead <razorhead@Mental.local>
2014-04-06 18:22:39 -0700

    controller changed for better readability

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -45,95 +45,113 @@ exports.all = function (req, res) {
             rank,
             radius           
  */
-exports.near = function (req, res) {
+var saveRestaurant = function (err, data) {
+    if (err) throw err;
+
+    restObject.location.push(data.results[0].geometry.location.lng);
+    restObject.location.push(data.results[0].geometry.location.lat);
+
+    Restaurant.update({
+        phone: filteredRestaurant.phone
+    }, restObject, {
+        upsert: true
+    }, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+
+    result.push(restObject);
+    callback();
+
+};
+
+
+var getYelpData = function (err, data) {
+    if (err) {
+        throw err;
+    }
+    //console.log(data.businesses[0].location);
+    var result = [];
+    // we have to use async here because
+    _.each(data.businesses,
+        function (restaurant, callback){
+            filterYelpData(restaurant, callback);
+        },
+        function (err) {
+            if (err) throw err;
+            res.jsonp(result);
+        });
+
+};
+var filterYelpData = function (restaurant, callback) {
+
+    var filteredRestaurant = {};
+
+    //name
+    filteredRestaurant.name = restaurant.name;
+    //url
+    filteredRestaurant.url = restaurant.url;
+    //distance
+    filteredRestaurant.distance = (restaurant.distance * MTR_TO_MILE).toFixed(2);
+    //rating_img_url
+    filteredRestaurant.rating_img_url = restaurant.rating_img_url;
+    //review_count
+    filteredRestaurant.review_count = restaurant.review_count;
+    //display_address
+    filteredRestaurant.address_display = restaurant.location.display_address;
+    //phone
+    filteredRestaurant.phone = restaurant.phone;
+    //category
+    filteredRestaurant.category = restaurant.categories[0][0];
+
+    // insert into the database after getting the 
+    //ref: http://stackoverflow.com/questions/7267102/how-do-i-update-upsert-a-document-in-mongoose
+    var rest = new Restaurant(filteredRestaurant),
+        restObject = rest.toObject();
+    delete restObject._id;
+
+    geocoder.geocode(filteredRestaurant.address_display[0] + filteredRestaurant.address_display[1],
+        saveRestaurant(err, data));
+
+};
+function getRestaurants(err, docs, res) {
+
+    if (err)
+        throw err;
+    if (!_.isEmpty(docs)) {
+        console.log('get data from mongodb');
+        res.jsonp(docs);
+    }
+    // no result in the database, make a call to the yelp API
+    else {
+        console.log('get data from yelp API');
+        var term = req.params.lon + ',' + req.params.lat;
+        // we have to limit the result to 5 because of Google Maps API's OVER_QUERY_LIMIT error
+        yelp.search({
+            term: "food",
+            ll: term,
+            radius_filter: "8000", // 5 miles
+            limit: 5
+        },
+        function (err, data) {
+            getYelpData(err, data);
+        });
+    }
+};
+
+var near = function (req, res) {
 
     Restaurant.find({
         location: {
             '$near': [parseFloat(req.params.lon), parseFloat(req.params.lat)]
         }
-    }, function (err, docs) {
-
-        if (err)
-            throw err;
-        if (!_.isEmpty(docs)) {
-            console.log('get data from mongodb');
-            res.jsonp(docs);
-        }
-        // no result in the database, make a call to the yelp API
-        else {
-            console.log('get data from yelp API');
-            var term = req.params.lon + ',' + req.params.lat;
-            // we have to limit the result to 5 because of Google Maps API's OVER_QUERY_LIMIT error
-            yelp.search({
-                term: "food",
-                ll: term,
-                radius_filter: "8000", // 5 miles
-                limit: 5
-            }, function (err, data) {
-                if (err) {
-                    throw err;
-                }
-                //console.log(data.businesses[0].location);
-                var result = [];
-                async.each(data.businesses, function (restaurant, callback) {
-
-                    var filteredRestaurant = {};
-
-                    //name
-                    filteredRestaurant.name = restaurant.name;
-                    //url
-                    filteredRestaurant.url = restaurant.url;
-                    //distance
-                    filteredRestaurant.distance = (restaurant.distance * MTR_TO_MILE).toFixed(2);
-                    //rating_img_url
-                    filteredRestaurant.rating_img_url = restaurant.rating_img_url;
-                    //review_count
-                    filteredRestaurant.review_count = restaurant.review_count;
-                    //display_address
-                    filteredRestaurant.address_display = restaurant.location.display_address;
-                    //phone
-                    filteredRestaurant.phone = restaurant.phone;
-                    //category
-                    filteredRestaurant.category = restaurant.categories[0][0];
-
-                    // insert into the database after getting the 
-                    //ref: http://stackoverflow.com/questions/7267102/how-do-i-update-upsert-a-document-in-mongoose
-                    var rest = new Restaurant(filteredRestaurant),
-                        restObject = rest.toObject();
-                    delete restObject._id;
-
-                    geocoder.geocode(filteredRestaurant.address_display[0] + filteredRestaurant.address_display[1],
-                        function (err, data) {
-                            if (err) throw err;
-
-                            restObject.location.push(data.results[0].geometry.location.lng);
-                            restObject.location.push(data.results[0].geometry.location.lat);
-
-                            Restaurant.update({
-                                phone: filteredRestaurant.phone
-                            }, restObject, {
-                                upsert: true
-                            }, function (err) {
-                                if (err) {
-                                    throw err;
-                                }
-                            });
-
-                            result.push(restObject);
-                            callback();
-
-                        });
-
-                }, function (err) {
-                    if (err) throw err;
-                    res.jsonp(result);
-                });
-
-            });
-        }
+    }, function(err, data) {
+        getRestaurants(err, data, res);
     });
-
 };
+exports.near = near;
+
 
 /**
  * List of Restaurants close by
